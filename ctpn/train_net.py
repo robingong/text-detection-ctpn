71afc35da2d69b277e8187876d88e0bdba0bd085
robingong <110605035@qq.com>
2018-12-20 11:20:14 +0800

    fix: File "/home/alex/source/text-detection-ctpn/lib/fast_rcnn/train.py", line 124, in train_model     raise Exception('Check your pretrained model {:s}'.format(self.pretrained_model)) Exception: Check your pretrained model data/pretrain/VGG_imagenet.npy

--- ctpn/train_net.py
+++ ctpn/train_net.py
@@ -30,6 +30,6 @@ if __name__ == '__main__':
     train_net(network, imdb, roidb,
               output_dir=output_dir,
               log_dir=log_dir,
-              pretrained_model='data/pretrain/VGG_imagenet.npy',
+              pretrained_model='/home/alex/source/text-detection-ctpn/data/pretrain/VGG_imagenet.npy', # data/pretrain/VGG_imagenet.npy
               max_iters=int(cfg.TRAIN.max_steps),
               restore=bool(int(cfg.TRAIN.restore)))
