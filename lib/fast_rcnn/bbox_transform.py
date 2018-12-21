8d86e0868c2cdf453f24c2bbbfc3ed89a2650c76
robingong <110605035@qq.com>
2018-12-21 14:57:34 +0800

    NAN

--- lib/fast_rcnn/bbox_transform.py
+++ lib/fast_rcnn/bbox_transform.py
@@ -28,8 +28,8 @@ def bbox_transform(ex_rois, gt_rois):
     targets_dw = np.log(gt_widths / ex_widths)
     # alex
     try:
-        if gt_heights / ex_heights <=0 :
-            print("-->>log(",gt_heights, "/", ex_heights,")")
+        # if gt_heights / ex_heights <=0 :
+        #     print("-->>log(",gt_heights, "/", ex_heights,")")
 
         targets_dh = np.log(gt_heights / ex_heights)
     except : #RuntimeWarning:
